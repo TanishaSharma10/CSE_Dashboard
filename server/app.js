b60f4cb7826fe1ba276c95826d28ffd0c9886bf9
TanishaSharma10 <tsharmamnc@gmail.com>
2024-08-25 09:50:08 +0530

    Another Add

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path=require("path");
 const mongoose = require("mongoose");
 
 require("./db/conn");
@@ -16,8 +17,14 @@ app.use(router);
 
 const port =8005;
 
+
 app.listen(port,()=>{
     console.log('server is running on port number {port}');
 });
+
+app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "client", "build")));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 
 DefaultData();
